28a391b7660c6b9bb9a1d6b0dc900b4cd65ad3c6
Fernando Gomes <162467862+fernando-gpadilha@users.noreply.github.com>
2024-05-01 21:54:06 -0300

    slide

--- acessets/index.js
+++ acessets/index.js
@@ -1,3 +1,9 @@
+let count = 1;
+document.getElementById('radio1').checked = true;
+setInterval(function () {
+    nextImage()
+}, 5000)
+
 function menuShow() {
     let menuMobile = document.querySelector('.mobile_menu');
     if (menuMobile.classList.contains('open')) { 
@@ -8,4 +14,12 @@ function menuShow() {
         menuMobile.classList.add('open');
         document.querySelector('icon').src = '#';
     }
+}
+
+function nextImage() {
+    count++;
+    if(count > 3){
+        count = 1;
+    }
+    document.getElementById('radio' + count).checked= true;
 }
